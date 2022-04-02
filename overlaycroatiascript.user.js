58011b61277714e7609f52ff4d83b5a6e5ab58b5
Grqfes <102879225+Grqfes@users.noreply.github.com>
2022-04-02 22:28:02 +0200

    Create overlaycroatiascript.user.js

---
+++ overlaycroatiascript.user.js
@@ -0,0 +1,14 @@
+if (window.top !== window.self) {
+    window.addEventListener('load', () => {
+            document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
+        (function () {
+            const i = document.createElement("img");
+            i.src = "https://i.ibb.co/SJf1HW0/dotted-place-template-croatia.png";
+            i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 2000px;height: 1000px;";
+            console.log(i);
+            return i;
+        })())
+
+    }, false);
+
+}
